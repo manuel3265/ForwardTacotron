9dbb4f3611ce30546658386a6d42f699a9525210
Manuel García <manuel.garcia02@uptc.edu.co>
2021-02-23 21:39:03 -0500

    Update Hparams

--- hparams.py
+++ hparams.py
@@ -103,9 +103,9 @@ tts_stop_threshold = -11           # Value below which audio generation ends.
 # Training
 
 tts_schedule = [(10,  1e-3,  10_000,  64),   # progressive training schedule
-                (5,  1e-4, 20_000,  64),   # (r, lr, step, batch_size)
-                (2,  1e-4, 30_000,  64),
-                (1,  1e-4, 50_000,  64)]
+                (5,  1e-4, 20_000,  32),   # (r, lr, step, batch_size)
+                (2,  1e-4, 30_000,  16),
+                (1,  1e-4, 50_000,  16)]
 
 tts_max_mel_len = 1250              # if you have a couple of extremely long spectrograms you might want to use this
 tts_clip_grad_norm = 1.0            # clips the gradient norm to prevent explosion - set to None if not needed
